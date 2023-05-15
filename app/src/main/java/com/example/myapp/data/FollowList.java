eb6469ce66ef7b50412b38d3cb94bab81345ddef
王陆离 <2970012705@qq.com>
2023-05-15 20:07:22 +0800

    Space页面初步完成

---
+++ app/src/main/java/com/example/myapp/data/FollowList.java
@@ -0,0 +1,71 @@
+package com.example.myapp.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class FollowList implements Parcelable {
+    private ArrayList<Follow> list;
+
+    public FollowList() {
+        list = new ArrayList<>();
+    }
+
+    private FollowList(Parcel in) {
+        list = in.createTypedArrayList(Follow.CREATOR);
+    }
+
+    public void insert(Follow follow) {
+        list.add(follow);
+    }
+
+    public void insert(int index, Follow follow) {
+        list.add(index, follow);
+    }
+
+    public void delete(int index) {
+        list.remove(index);
+    }
+
+    public Follow get(int index) {
+        return list.get(index);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<FollowList of length %d>", list.size());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeTypedList(list);
+    }
+
+    public static Creator<FollowList> CREATOR = new Creator<FollowList>() {
+        @Override
+        public FollowList createFromParcel(Parcel source) {
+            return new FollowList(source);
+        }
+
+        @Override
+        public FollowList[] newArray(int size) {
+            return new FollowList[size];
+        }
+    };
+}
