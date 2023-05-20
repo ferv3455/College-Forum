1a4f436f31d7d2710235c7dbe7a47b7f8dd7d52c
王陆离 <2970012705@qq.com>
2023-05-20 14:09:11 +0800

    从后端api获取显示当前用户·用户名、头像以及个性签名

--- app/src/main/java/com/example/myapp/activity/SpaceActivity.java
+++ app/src/main/java/com/example/myapp/activity/SpaceActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapp.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,16 +14,29 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapp.R;
+import com.example.myapp.connection.HTTPRequest;
+import com.example.myapp.connection.TokenManager;
 import com.example.myapp.fragment.home.PostListFragment;
 import com.example.myapp.fragment.space.FollowListFragment;
 import com.google.android.material.tabs.TabLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.BreakIterator;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class SpaceActivity extends AppCompatActivity {
 
     // ... other fields ...
 
     private Fragment fragment;
-    private String username;
+    private String currentUsername;
+    private String currentToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +45,51 @@ public class SpaceActivity extends AppCompatActivity {
 
         ImageView userAvatar = findViewById(R.id.user_avatar);
         TextView userName = findViewById(R.id.user_name);
+        TextView userDescription = findViewById(R.id.user_description);  // assuming you have this in your layout
 
         // Set user avatar and name
-        userAvatar.setImageResource(R.drawable.avatar); // Replace with actual user avatar
-        userName.setText("Username"); // Replace with actual username
+        currentToken = TokenManager.getSavedToken(this);
+        currentUsername = TokenManager.getSavedUsername(this);
+
+        HTTPRequest.get("account/profile/" + currentUsername, currentToken, new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        // handle the error
+                        e.printStackTrace();
+                        userAvatar.setImageResource(R.drawable.avatar); // Replace with actual user avatar
+                        userName.setText(currentUsername); // Replace with actual username
+                        Intent intent = new Intent(SpaceActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            try {
+                                String responseBody = response.body().string();
+                                JSONObject jsonObject = new JSONObject(responseBody);
+                                String avatarBase64 = jsonObject.getString("avatar");
+                                String description = jsonObject.getString("description");
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Decode base64 string to a Bitmap
+                                        byte[] decodedString = Base64.decode(avatarBase64, Base64.DEFAULT);
+                                        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                                        userAvatar.setImageBitmap(decodedByte);
+                                        userName.setText(currentUsername);
+                                        userDescription.setText(description);
+                                    }
+                                });
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
 
         fragment = PostListFragment.newInstance("time");
 
@@ -47,10 +105,10 @@ public class SpaceActivity extends AppCompatActivity {
                         fragment = PostListFragment.newInstance("time");
                         break;
                     case 2:
-                        fragment = new FollowListFragment(1, username);
+                        fragment = new FollowListFragment(1, currentUsername);
                         break;
                     case 3:
-                        fragment = new FollowListFragment(2, username);
+                        fragment = new FollowListFragment(2, currentUsername);
                         break;
                     default:
                         throw new IllegalStateException("Unexpected position: " + tab.getPosition());
